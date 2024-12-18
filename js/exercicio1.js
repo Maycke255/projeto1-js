b408baf338eec1fc1d2873526e5b7fd0b836b9e0
Maycke255 <reydsonk@gmail.com>
2024-12-18 16:12:16 -0300

    Alterações no html e JS, completos.

--- js/exercicio1.js
+++ js/exercicio1.js
@@ -1,16 +1,24 @@
 function salvarDados() {
-let nome = document.getElementById("name").value;
-let sobrenome = document.getElementsByClassName("surname").value;
-let estudo = document.getElementById("study").value;
-let nascimento = document.getElementById("birth").value
+    // Obtém os valores dos campos do formulário
+    let nome = document.getElementById("name1").value;
+    let sobrenome = document.getElementsByClassName("input")[1].value;
+    let estudo = document.getElementById("study").value;
+    let nascimento = parseInt(document.getElementById("birth").value);
 
-console.log("nome");
-console.log("sobrenome");
-console.log("estudo");
-console.log("nascimento")
+    // Calcula o ano atual e a idade
+    const anoAtual = new Date().getFullYear(); // Corrigido com os parênteses
+    const idade = anoAtual - nascimento;
 
-const anoAtual = new Date().getFullYear;
-const idade = anoAtual - nascimento;
-
-botaoSalvar.addEventListener("click", salvarDados);
+    // Exibe os dados salvos no alerta
+    alert(
+        "Seus dados foram salvos!" + "\n" +
+        "Nome: " + nome + " " + sobrenome + "\n" +
+        "Campo de estudo: " + estudo + "\n" +
+        "Idade: " + idade + "\n" // Exibe a idade calculada
+    );
 }
+
+// Associa o evento de clique ao botão
+const botaoSalvar = document.getElementById("botaoSalvar");
+botaoSalvar.addEventListener("click", salvarDados);
+
